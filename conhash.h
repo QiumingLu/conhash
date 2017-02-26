b1ed37af5b87ff16357397cabb3d7f34999ef895
QiumingLu <cming_cs@163.com>
2017-02-26 12:11:42 +0800

    add copyright

--- conhash.h
+++ conhash.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 Mirants Lu. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef CONHASH_H_
 #define CONHASH_H_
 
